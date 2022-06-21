a1f6d946676fb3cfdd34af831f65c27429d69094
krzysiekn111 <krzysiekn111@gmail.com>
2022-06-21 10:40:01 +0200

    all

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,13 +1,17 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/tasks")
 public class TaskController {
+    @GetMapping
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
