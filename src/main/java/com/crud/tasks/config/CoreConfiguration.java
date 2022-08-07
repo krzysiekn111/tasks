e16f84f70ff8da2913635416c90e43a265e1eea0
krzysiekn111 <krzysiekn111@gmail.com>
2022-08-07 18:57:22 +0200

    23.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -4,26 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-@Configuration
-@EnableSwagger2
+
+
+
 @EnableWebMvc
+@Configuration
 public class CoreConfiguration {
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.crud_todolist.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+
+
 }
