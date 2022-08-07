d5861f2501e24d8b5e5e73bebc067f7a191c597f
krzysiekn111 <krzysiekn111@gmail.com>
2022-08-07 10:30:00 +0200

    23.2. Instalacja Swaggera, czyli dokumentacja dla API

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -4,18 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-
-
-@EnableWebMvc
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
+@EnableSwagger2
+@EnableWebMvc
 public class CoreConfiguration {
-
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
-
-
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.crud_todolist.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
